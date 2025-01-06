03905a115102f6718d7454e60f112a0bf85d1539
AnupamSingha <singharocky00@gmail.com>
2025-01-06 13:28:49 +0000

    Add files via upload

---
+++ routes/posts.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  caption: String,
+  like: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  }],
+  comments: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+      text: String,
+      date: Date,
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  shares: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  }],
+  picture: String
+})
+
+
+module.exports = mongoose.model("post", postSchema);
